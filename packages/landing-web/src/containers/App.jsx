68c93b034c70bae82e33d5169d13c46579d5566b
Noro <ziyi@gmail.com>
2018-08-15 22:20:26 +0800

    change welcome

--- packages/landing-web/src/containers/App.jsx
+++ packages/landing-web/src/containers/App.jsx
@@ -17,10 +17,12 @@ export class App extends PureComponent {
 
   render() {
     const { user } = this.props;
+    console.log('====================================');
+    console.log(`${user}`);
+    console.log('====================================');
     return (
       <div className="demo-app">
-        Hello,
-        {user}
+        {`Hello, ${user}!`}
       </div>
     );
   }
